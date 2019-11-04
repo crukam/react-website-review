6c901e8925c1ac083df952d44a1eb86bdecf2c6e
Claude Rukamiza <jnkurunz0912@gmail.com>
2019-11-04 22:34:56 +0000

    my comment input displayed

--- src/comment.jsx
+++ src/comment.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
-
-import './comment.css'
+import CommentInput from './commentInput.jsx';
+import './comment.css';
 class Comment extends React.Component{
+    constructor(props){
+        super(props);
+        this.handledisplayInput=this.handledisplayInput.bind(this);
+        this.state={showcommentInput:false}
+    }
+    handledisplayInput(){
+        this.setState({showcommentInput:true})
+    }
     
     render(){
         let list=[];
@@ -9,8 +17,11 @@ class Comment extends React.Component{
             list.push(<p className="comment-item" key={index}>{item.comment}</p>)})
         return(
             <div className="comment-wrapper">
-                
-            {list}
+                <h3 className="myresto-title">custumers feedback</h3>
+               {list}
+               {this.state.showcommentInput?<CommentInput></CommentInput>:null}
+               <button onClick={this.handledisplayInput}>add comment</button>
+               
             </div>
         )
     }
---
+++ src/commentInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './comment.css';
+class CommentInput extends React.Component{
+    render(){
+        return(
+        <div className="input-wrapper">
+            <div>I am here do not search aigain</div>
+        </div>
+        );
+    }
+}
+export default CommentInput;
