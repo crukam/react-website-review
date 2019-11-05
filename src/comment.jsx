9ddc2af3611d8237b2a94067115006cb88514e1b
Claude Rukamiza <jnkurunz0912@gmail.com>
2019-11-05 15:52:19 +0000

    my retrieve version

--- src/comment.jsx
+++ src/comment.jsx
@@ -4,14 +4,19 @@ import './comment.css';
 class Comment extends React.Component{
     constructor(props){
         super(props);
+        this.handlesaveInput=this.handlesaveInput.bind(this);
         this.handledisplayInput=this.handledisplayInput.bind(this);
         this.state={showcommentInput:false}
     }
     handledisplayInput(){
         this.setState({showcommentInput:true})
     }
+    handlesaveInput(){
+        this.setState({showcommentInput:false})
+    }
     
     render(){
+        console.log(this.state.showcommentInput);
         let list=[];
         this.props.ratings.forEach((item,index)=>{
             list.push(<p className="comment-item" key={index}>{item.comment}</p>)})
@@ -19,8 +24,8 @@ class Comment extends React.Component{
             <div className="comment-wrapper">
                 <h3 className="myresto-title">custumers feedback</h3>
                {list}
-               {this.state.showcommentInput?<CommentInput></CommentInput>:null}
-               <button onClick={this.handledisplayInput}>add comment</button>
+               {this.state.showcommentInput?<CommentInput onClick={this.handlesaveInput}></CommentInput>:null}
+               {this.state.showcommentInput?null:<button onClick={this.handledisplayInput}>add comment</button>}
                
             </div>
         )
--- src/commentInput.jsx
+++ src/commentInput.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import './comment.css';
 class CommentInput extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={star: 0,comment:''}
+    }
+    handlestarchange(e){
+      this.setState({star:e.target.value});
+    }
+    handlecommentchange(e){
+
+    }
+    
     render(){
         return(
         <div className="input-wrapper">
-            <div>I am here do not search aigain</div>
+            
+           
+			
+            <div className = "inputcontainer rating">
+                     Ratings
+					<select onChange={this.handlestarchange} >
+					<option> </option>
+					<option >0 </option>  
+					<option >1</option>  
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+                    <option>5</option>  
+					</select> 
+			</div>
+            <div className = "inputcontainer" onChange={this.handlecommentchange}>
+                <textarea className ="InputItem" rows="4" cols="16" placeholder="comment">
+				</textarea>
+			</div>
+            
+            <button onClick={this.props.onClick}>save</button>
         </div>
         );
     }
