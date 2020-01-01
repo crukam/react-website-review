8c6c0ccb9771fe86aa57372f892806079022308b
Claude Rukamiza <jnkurunz0912@gmail.com>
2020-01-01 23:51:03 +0000

    Updates

---
+++ precache-manifest.4c443f5a40580a0c9af9c24eb0b94479.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c57f3358846d2f1d16756727e40fa984",
+    "url": "/react-website-review/index.html"
+  },
+  {
+    "revision": "a4e97ef8266796dd58e4",
+    "url": "/react-website-review/static/css/main.d5a8665e.chunk.css"
+  },
+  {
+    "revision": "a20acb3c7c820a3425c0",
+    "url": "/react-website-review/static/js/2.1a593dd1.chunk.js"
+  },
+  {
+    "revision": "a4e97ef8266796dd58e4",
+    "url": "/react-website-review/static/js/main.23bbadce.chunk.js"
+  },
+  {
+    "revision": "5af10a563c7f97ec6aa2",
+    "url": "/react-website-review/static/js/runtime-main.5a15c556.js"
+  }
+]);
